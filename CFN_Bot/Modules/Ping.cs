8a6ba21b621aed6c332518e84bdb4bc198d20c1e
thoma <thoma@A>
2019-01-24 23:54:11 +1000

    Add project files.

---
+++ CFN_Bot/Modules/Ping.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace CFN_Bot.Modules
+{
+	public class Ping : ModuleBase<SocketCommandContext>
+	{
+		[Command("ping")]
+
+		public async Task PingAsync()
+		{
+			await ReplyAsync($"type yes or no");			
+		}
+
+
+		[Command("read")]
+		public async Task Read(string name)
+		{
+			await ReplyAsync($"{name} ");
+		}
+
+
+		[Command("Gamedev")]
+		public async Task gamedev()
+		{
+			var user = Context.User;
+			var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "GameDev");
+			await ReplyAsync("You have been Assigned the Game Developer role!");
+			await (user as IGuildUser).AddRoleAsync(role);
+			
+		}
+
+		[Command("VideoCreator")]
+		public async Task youtuber()
+		{
+			var user = Context.User;
+			var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "VideoCreator");
+			await ReplyAsync("You have been Assigned the VideoCreator role!");
+			await (user as IGuildUser).AddRoleAsync(role);
+		}
+
+		[Command("GamePress")]
+		public async Task streamer()
+		{
+			var user = Context.User;
+			var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "GamePress");
+			await ReplyAsync("You have been Assigned the GamePress role!");
+			await (user as IGuildUser).AddRoleAsync(role);
+		}
+
+		[Command("Artist")]
+		public async Task Artist()
+		{
+			var user = Context.User;
+			var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Artist");
+			await ReplyAsync("You have been Assigned the Artist role!");
+			await (user as IGuildUser).AddRoleAsync(role);
+		}
+
+		[Command("Music")]
+		public async Task Music()
+		{
+			var user = Context.User;
+			var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Music_And_Audio");
+			await ReplyAsync("You have been Assigned the Music role!");
+			await (user as IGuildUser).AddRoleAsync(role);
+		}
+
+		[Command("Streamer")]
+		public async Task Streamer()
+		{
+			var user = Context.User;
+			var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Streamer");
+			await ReplyAsync("You have been Assigned the Streamer role!");
+			await (user as IGuildUser).AddRoleAsync(role);
+		}
+
+
+		[Command("helpAdmin")]
+		public async Task helper()
+		{
+			var guild = Context.Guild;
+			var Channel = guild.DefaultChannel as IMessageChannel;
+			await Channel.SendMessageAsync($"Welcome, {Context.User.Mention}! welcome to CFN!, Please type one of the commands to set your role!");
+
+			EmbedBuilder builder = new EmbedBuilder();
+			builder.Color = new Color(1f, 1f, 0f);
+			builder.AddField("!GameDev", "if you design games, or work in a studio!", false);
+			builder.AddField("!VideoCreator", "if you are a content creator!", false);
+			builder.AddField("!GamePress", "text");
+			builder.AddField("!Artist", "text");
+			builder.AddField("!Music", "text");
+			builder.AddField("!Streamer", "text");
+			await Channel.SendMessageAsync(null, false, builder.Build());
+		}
+
+		[Command("Roll")]
+		public async Task roller()
+		{
+			Random rnd = new Random();
+			await ReplyAsync($"You Rolled a { rnd.Next(0, 12) }!"); 
+		}
+
+	}
+}
---
+++ CFN_Bot/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace CFN_Bot
+{
+	class Program
+	{
+
+		static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+		private DiscordSocketClient Client;
+		private CommandService Commands;
+		private IServiceProvider Services;
+
+		public async Task RunBotAsync()
+		{
+			Client = new DiscordSocketClient();
+			Commands = new CommandService();
+			Services = new ServiceCollection()
+				.AddSingleton(Client)
+				.AddSingleton(Commands)
+				.BuildServiceProvider();
+
+			string botKey = "NTM0MzI4NzkyOTkxMTM3Nzkz.Dx4ogw.4MUKzsrxUvVqIZfBsOlos0xXtC8";
+
+			//event subs
+			Client.Log += Log;
+			Client.UserJoined += AnnounceUserJoined;
+			
+
+			await RegisterCommandsAsync();
+
+			await Client.LoginAsync(TokenType.Bot, botKey);
+			await Client.SetGameAsync("With a Ball", null, ActivityType.Playing);
+			await Client.StartAsync();
+
+			await Task.Delay(-1);
+		}
+		
+		private Task Log(LogMessage arg)
+		{
+			Console.WriteLine(arg);
+			return Task.CompletedTask;
+		}
+
+		private async Task AnnounceUserJoined(SocketGuildUser user)
+		{
+			var guild = user.Guild;
+
+
+			//TODO CHANGE TO ANNOUNCEMENT CHANNEL
+			var Channel = guild.DefaultChannel;
+			await Channel.SendMessageAsync($"Welcome, {user.Mention}! welcome to CFN!, Please type one of the commands to set your role!");
+
+			EmbedBuilder builder = new EmbedBuilder();
+			builder.Color = new Color(1f, 1f, 0f);
+			builder.AddField("Commands", "!gamedev >>> Gamedev Role !youtuber >>> Youtuber Role", false);
+			builder.AddField("", "Tester", false);
+			await Channel.SendMessageAsync(null,false,builder.Build());
+					
+		}
+
+		public async Task RegisterCommandsAsync()
+		{
+			Client.MessageReceived += HandleCommandAsync;
+			await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
+		}
+
+		private async Task HandleCommandAsync(SocketMessage arg)
+		{
+			var message = arg as SocketUserMessage;
+
+			if (message == null || message.Author.IsBot) return;
+
+			int argPos = 0; 
+
+			if(message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(Client.CurrentUser, ref argPos))
+			{
+				var context = new SocketCommandContext(Client, message);
+
+				var result = await Commands.ExecuteAsync(context, argPos, Services);
+
+				if (!result.IsSuccess)
+					Console.WriteLine(result.ErrorReason);
+			}
+
+		}
+	}
+}
